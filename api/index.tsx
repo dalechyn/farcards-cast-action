be6e5d1b0abd62be97485ba62ff274f8f5f6e0a3
hotwater <vladyslav@spilnota.xyz>
2024-04-27 01:59:20 +0300

    feat: add basic frame

--- api/index.tsx
+++ api/index.tsx
@@ -1,4 +1,4 @@
-import { Frog } from 'frog'
+import { Button, Frog } from 'frog'
 import { devtools } from 'frog/dev'
 import { serveStatic } from 'frog/serve-static'
 import { neynar } from 'frog/hubs'
@@ -23,6 +23,41 @@ export const app = new Frog({
   )
 
 
+app.frame('/install', (c) => {
+return c.res({image:(
+        <div
+          tw="flex"
+          style={{
+            alignItems: 'center',
+            background: 'linear-gradient(to right, #432889, #17101F)',
+            backgroundSize: '100% 100%',
+            flexDirection: 'column',
+            flexWrap: 'nowrap',
+            height: '100%',
+            justifyContent: 'center',
+            textAlign: 'center',
+            width: '100%',
+          }}
+        >
+          <div
+            style={{
+              color: 'white',
+              fontSize: 60,
+              fontStyle: 'normal',
+              letterSpacing: '-0.025em',
+              lineHeight: 1.4,
+              marginTop: 30,
+              padding: '0 120px',
+              whiteSpace: 'pre-wrap',
+            }}
+          >
+        Install "Buy farcard" action (mobile only).
+          </div>
+        </div>
+      ),
+    intents: [<Button.AddCastAction action='/'>Install</Button.AddCastAction>]
+  })
+})
 app.castAction('/', (c) => {
   return c.frame({action:`https://far.cards/${c.var.interactor?.fid}`})
 }, {
